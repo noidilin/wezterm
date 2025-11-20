28bd7167ce04ea41a22f689d7bb48b4141c6d08d
noidilin <linganinja.0120@gmail.com>
2025-11-21 00:06:00 +0800

    fix(launch): set domain to local in other launch options for windows
    - in previous setup only 'WSL Arch' has domain setup
      - other option still pass args only
      - thus they can not escape the 'WSL:Arch' domain

--- config/launch.lua
+++ config/launch.lua
@@ -9,10 +9,14 @@ if platform.is_win then
    options.default_prog = { 'nu' }
    options.launch_menu = {
       { label = 'WSL Arch', domain = { DomainName = 'WSL:Arch' } },
-      { label = 'PowerShell Core', args = { 'pwsh', '-NoLogo' } },
-      { label = 'Command Prompt', args = { 'cmd' } },
-      { label = 'Nushell', args = { 'nu' } },
-      { label = 'PowerShell Desktop', args = { 'powershell' } },
+      { label = 'Nushell', domain = { DomainName = 'local' }, args = { 'nu' } },
+      {
+         label = 'PowerShell',
+         domain = { DomainName = 'local' },
+         args = { 'pwsh', '-NoLogo' },
+      },
+      { label = 'Command Prompt', domain = { DomainName = 'local' }, args = { 'cmd' } },
+      { label = 'Windows PowerShell', domain = { DomainName = 'local' }, args = { 'powershell' } },
    }
 elseif platform.is_mac then
    options.default_prog = { 'zsh', '-l' }
