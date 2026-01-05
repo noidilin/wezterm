25e03a1328fabc0a8c53b2ffcc02efc645bc739f
noidilin <linganinja.0120@gmail.com>
2026-01-05 16:07:51 +0800

    feat(fonts): adjust font size in macOS

--- config/fonts.lua
+++ config/fonts.lua
@@ -3,8 +3,8 @@ local platform = require('utils.platform')
 
 -- local font_family = 'Maple Mono Normal NF CN'
 local font_family = 'CommitMono Nerd Font Mono'
-local font_size = platform.is_win and 12.5 or 12
-local line_height = platform.is_win and 1.5 or 1.2
+local font_size = platform.is_win and 12.5 or 14
+local line_height = platform.is_win and 1.5 or 1.3
 
 return {
    font = wezterm.font({
