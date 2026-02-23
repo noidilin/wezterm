fbf68d94175169cad8e2d6099a7970dc355b9f0b
noidilin <linganinja.0120@gmail.com>
2026-02-24 03:24:04 +0800

    fix: remove some of the updates from rebasing

--- colors/custom.lua
+++ colors/custom.lua
@@ -1,68 +1,63 @@
 -- A slightly altered version of catppucchin mocha
 local colorscheme = {
-   foreground = '#b3b3b3',
-   background = '#191919',
-   compose_cursor = '#9d9d9d',
-   cursor_fg = '#eaeaea',
-   cursor_bg = '#878787',
-   cursor_border = '#878787',
-   selection_bg = '#303030',
-   selection_fg = '#9d9d9d',
-   -- order: black > red > green > yellow > blue > magenta > cyan > white
-   ansi = { '#474747', '#b07878', '#778777', '#d6caab', '#7d96ad', '#797994', '#769494', '#dad5c8' },
-   brights = {
-      '#5d5d5d',
-      '#cc9393',
-      '#9bb09b',
-      '#ebd6b7',
-      '#9db2cf',
-      '#9f9fbd',
-      '#92b3b3',
-      '#faf5eb',
-   },
-   -- further customization
-   cursor_border = '#878787',
-   selection_fg = '#8e8e8e',
-   cursor_fg = '#eaeaea',
-   scrollbar_thumb = '#4e4e4e',
+  foreground = '#b3b3b3',
+  background = '#191919',
+  compose_cursor = '#9d9d9d',
+  cursor_fg = '#eaeaea',
+  cursor_bg = '#878787',
+  cursor_border = '#878787',
+  selection_bg = '#303030',
+  selection_fg = '#9d9d9d',
+  -- order: black > red > green > yellow > blue > magenta > cyan > white
+  ansi = { '#474747', '#b07878', '#778777', '#d6caab', '#7d96ad', '#797994', '#769494', '#dad5c8' },
+  brights = {
+    '#5d5d5d',
+    '#cc9393',
+    '#9bb09b',
+    '#ebd6b7',
+    '#9db2cf',
+    '#9f9fbd',
+    '#92b3b3',
+    '#faf5eb',
+  },
+  scrollbar_thumb = '#4e4e4e',
 
-   tab_bar = {
-      -- The color of the strip that goes along the top of the window
-      -- (does not apply when fancy tab bar is in use)
-      background = '#191919',
-      active_tab = {
-         bg_color = '#242424', -- background color
-         fg_color = '#9d9d9d', -- text color
-         intensity = 'Bold', -- default is "Normal" (options: "Half", "Normal" or "Bold")
-         underline = 'Single', -- default is "None" (options: "None", "Single" or "Double")
-         italic = false, -- default is false
-         strikethrough = false, -- default is false
-      },
+  tab_bar = {
+    -- The color of the strip that goes along the top of the window
+    -- (does not apply when fancy tab bar is in use)
+    background = '#191919',
+    active_tab = {
+      bg_color = '#242424',     -- background color
+      fg_color = '#9d9d9d',     -- text color
+      intensity = 'Bold',       -- default is "Normal" (options: "Half", "Normal" or "Bold")
+      underline = 'Single',     -- default is "None" (options: "None", "Single" or "Double")
+      italic = false,           -- default is false
+      strikethrough = false,    -- default is false
+    },
 
-      --[[ inactive_tab = { -- same options as `active_tab` section
+    --[[ inactive_tab = { -- same options as `active_tab` section
 			bg_color = "#191919",
 			fg_color = "#4e4e4e",
 			italic = true,
 		}, ]]
 
-      --[[ inactive_tab_hover = { -- same options as `active_tab` section
+    --[[ inactive_tab_hover = { -- same options as `active_tab` section
 			bg_color = "#191919",
 			fg_color = "#5d5d5d",
 			italic = true,
 		}, ]]
 
-      new_tab = { -- same options as `active_tab` section
-         bg_color = '#191919',
-         fg_color = '#4e4e4e',
-      },
+    new_tab = {   -- same options as `active_tab` section
+      bg_color = '#191919',
+      fg_color = '#4e4e4e',
+    },
 
-      new_tab_hover = { -- same options as `active_tab` section
-         bg_color = '#191919',
-         fg_color = '#5d5d5d',
-      },
-   },
-   split = '#353535',
-   compose_cursor = '#5d5d5d',
+    new_tab_hover = {   -- same options as `active_tab` section
+      bg_color = '#191919',
+      fg_color = '#5d5d5d',
+    },
+  },
+  split = '#353535',
 }
 
 return colorscheme
--- config/bindings.lua
+++ config/bindings.lua
@@ -38,85 +38,85 @@ end
 
 -- stylua: ignore
 local keys = {
-   -- Remove default bindings
-   { key = 'n', mods = 'CTRL', action = act.DisableDefaultAssignment },
-   { key = 'h', mods = 'CTRL', action = act.DisableDefaultAssignment },
-   { key = 'j', mods = 'CTRL', action = act.DisableDefaultAssignment },
-   { key = 'k', mods = 'CTRL', action = act.DisableDefaultAssignment },
-   { key = 'l', mods = 'CTRL', action = act.DisableDefaultAssignment },
-   { key = 't', mods = 'CTRL | SHIFT', action = act.DisableDefaultAssignment },
-   { key = 'w', mods = 'CTRL | SHIFT', action = act.DisableDefaultAssignment },
+  -- Remove default bindings
+  { key = 'n',        mods = 'CTRL',          action = act.DisableDefaultAssignment },
+  { key = 'h',        mods = 'CTRL',          action = act.DisableDefaultAssignment },
+  { key = 'j',        mods = 'CTRL',          action = act.DisableDefaultAssignment },
+  { key = 'k',        mods = 'CTRL',          action = act.DisableDefaultAssignment },
+  { key = 'l',        mods = 'CTRL',          action = act.DisableDefaultAssignment },
+  { key = 't',        mods = 'CTRL | SHIFT',  action = act.DisableDefaultAssignment },
+  { key = 'w',        mods = 'CTRL | SHIFT',  action = act.DisableDefaultAssignment },
 
-   -- HACK: resend space key stroke (https://github.com/wezterm/wezterm/issues/4055#issuecomment-1694542317)
-   { key = 'Space', mods = 'SHIFT', action = act.SendKey({ key = 'Space', mods = 'SHIFT' }), },
-   { key = 'Space', mods = 'CTRL', action = act.SendKey({ key = 'Space', mods = 'CTRL' }), },
+  -- HACK: resend space key stroke (https://github.com/wezterm/wezterm/issues/4055#issuecomment-1694542317)
+  { key = 'Space',    mods = 'SHIFT',         action = act.SendKey({ key = 'Space', mods = 'SHIFT' }), },
+  { key = 'Space',    mods = 'CTRL',          action = act.SendKey({ key = 'Space', mods = 'CTRL' }), },
 
-   -- paste from the clipboard
-   { key = 'V', mods = 'CTRL|SHIFT', action = act.PasteFrom 'Clipboard' },
+  -- paste from the clipboard
+  { key = 'V',        mods = 'CTRL|SHIFT',    action = act.PasteFrom 'Clipboard' },
 
-   { key = 'F1', mods = 'NONE', action = act.ActivateCopyMode },
-   { key = 'F3', mods = 'NONE', action = act.ActivateCommandPalette },
-   { key = 'F4', mods = 'NONE', action = act.ShowLauncherArgs({ flags = 'FUZZY|LAUNCH_MENU_ITEMS|DOMAINS' }) },
-   { key = 'F5', mods = 'NONE', action = act.ShowLauncherArgs({ flags = 'FUZZY|TABS' }) },
-   { key = 'F6', mods = 'NONE', action = act.ShowLauncherArgs({ flags = 'FUZZY|WORKSPACES' }), },
-   { key = 'F11', mods = 'NONE', action = act.ToggleFullScreen },
-   { key = 'F12', mods = 'NONE', action = act.ShowDebugOverlay },
-   -- { key = "phys:Space", mods = "LEADER",        action = act.ActivateCommandPalette },
+  { key = 'F1',       mods = 'NONE',          action = act.ActivateCopyMode },
+  { key = 'F3',       mods = 'NONE',          action = act.ActivateCommandPalette },
+  { key = 'F4',       mods = 'NONE',          action = act.ShowLauncherArgs({ flags = 'FUZZY|LAUNCH_MENU_ITEMS|DOMAINS' }) },
+  { key = 'F5',       mods = 'NONE',          action = act.ShowLauncherArgs({ flags = 'FUZZY|TABS' }) },
+  { key = 'F6',       mods = 'NONE',          action = act.ShowLauncherArgs({ flags = 'FUZZY|WORKSPACES' }), },
+  { key = 'F11',      mods = 'NONE',          action = act.ToggleFullScreen },
+  { key = 'F12',      mods = 'NONE',          action = act.ShowDebugOverlay },
+  -- { key = "phys:Space", mods = "LEADER",        action = act.ActivateCommandPalette },
 
-   -- Send original key stroke when pressing leader key twice
-   { key = 'q', mods = 'LEADER | CTRL', action = act.SendKey({ key = 'q', mods = 'CTRL' }) },
+  -- Send original key stroke when pressing leader key twice
+  { key = 'q',        mods = 'LEADER | CTRL', action = act.SendKey({ key = 'q', mods = 'CTRL' }) },
 
-   -- Scroll by half page
-   { key = 'PageUp', mods = 'SHIFT', action = act.ScrollByPage(-0.5) },
-   { key = 'PageDown', mods = 'SHIFT', action = act.ScrollByPage(0.5) },
+  -- Scroll by half page
+  { key = 'PageUp',   mods = 'SHIFT',         action = act.ScrollByPage(-0.5) },
+  { key = 'PageDown', mods = 'SHIFT',         action = act.ScrollByPage(0.5) },
 
-   -- Pane split
-   { key = '-', mods = 'LEADER', action = act.SplitVertical({ domain = 'CurrentPaneDomain' }) },
-   { key = '\\', mods = 'LEADER', action = act.SplitHorizontal({ domain = 'CurrentPaneDomain' }) },
+  -- Pane split
+  { key = '-',        mods = 'LEADER',        action = act.SplitVertical({ domain = 'CurrentPaneDomain' }) },
+  { key = '\\',       mods = 'LEADER',        action = act.SplitHorizontal({ domain = 'CurrentPaneDomain' }) },
 
-   -- Pane manipulate
-   { key = 'x', mods = 'LEADER', action = act.CloseCurrentPane({ confirm = false }) },
-   { key = 'z', mods = 'LEADER', action = act.TogglePaneZoomState },
-   { key = 'o', mods = 'LEADER', action = act.RotatePanes('Clockwise') },
+  -- Pane manipulate
+  { key = 'x',        mods = 'LEADER',        action = act.CloseCurrentPane({ confirm = false }) },
+  { key = 'z',        mods = 'LEADER',        action = act.TogglePaneZoomState },
+  { key = 'o',        mods = 'LEADER',        action = act.RotatePanes('Clockwise') },
 
-   -- Pane resize
-   { key = 'H', mods = 'LEADER|SHIFT', action = act.AdjustPaneSize({ 'Left', 5 }) },
-   { key = 'J', mods = 'LEADER|SHIFT', action = act.AdjustPaneSize({ 'Down', 5 }) },
-   { key = 'K', mods = 'LEADER|SHIFT', action = act.AdjustPaneSize({ 'Up', 5 }) },
-   { key = 'L', mods = 'LEADER|SHIFT', action = act.AdjustPaneSize({ 'Right', 5 }) },
+  -- Pane resize
+  { key = 'H',        mods = 'LEADER|SHIFT',  action = act.AdjustPaneSize({ 'Left', 5 }) },
+  { key = 'J',        mods = 'LEADER|SHIFT',  action = act.AdjustPaneSize({ 'Down', 5 }) },
+  { key = 'K',        mods = 'LEADER|SHIFT',  action = act.AdjustPaneSize({ 'Up', 5 }) },
+  { key = 'L',        mods = 'LEADER|SHIFT',  action = act.AdjustPaneSize({ 'Right', 5 }) },
 
-   -- Tab Manimupate
-   { key = 'c', mods = 'LEADER', action = act.SpawnTab('CurrentPaneDomain') },
-   { key = '&', mods = 'LEADER|SHIFT', action = act.CloseCurrentTab({ confirm = false }) },
+  -- Tab Manimupate
+  { key = 'c',        mods = 'LEADER',        action = act.SpawnTab('CurrentPaneDomain') },
+  { key = '&',        mods = 'LEADER|SHIFT',  action = act.CloseCurrentTab({ confirm = false }) },
 
-   -- Adjust tab order
-   { key = 'Tab', mods = 'CTRL|SHIFT', action = act.ActivateTabRelative(-1) },
-   { key = 'Tab', mods = 'CTRL', action = act.ActivateTabRelative(1) },
-   ---- Or shortcuts to move tab w/o move_tab table. SHIFT is for when caps lock is on
-   { key = '<', mods = 'LEADER|SHIFT', action = act.MoveTabRelative(-1) },
-   { key = '>', mods = 'LEADER|SHIFT', action = act.MoveTabRelative(1) },
+  -- Adjust tab order
+  { key = 'Tab',      mods = 'CTRL|SHIFT',    action = act.ActivateTabRelative(-1) },
+  { key = 'Tab',      mods = 'CTRL',          action = act.ActivateTabRelative(1) },
+  ---- Or shortcuts to move tab w/o move_tab table. SHIFT is for when caps lock is on
+  { key = '<',        mods = 'LEADER|SHIFT',  action = act.MoveTabRelative(-1) },
+  { key = '>',        mods = 'LEADER|SHIFT',  action = act.MoveTabRelative(1) },
 
-   -- Key table for moving tabs and resize pane
-   { key = 'm', mods = 'LEADER', action = act.ActivateKeyTable({ name = 'move_tab', one_shot = false }), },
-   { key = 's', mods = 'LEADER', action = act.ActivateKeyTable({ name = 'resize_pane', one_shot = false }), },
+  -- Key table for moving tabs and resize pane
+  { key = 'm',        mods = 'LEADER',        action = act.ActivateKeyTable({ name = 'move_tab', one_shot = false }), },
+  { key = 's',        mods = 'LEADER',        action = act.ActivateKeyTable({ name = 'resize_pane', one_shot = false }), },
 
-   -- smart-splits
-   ---- move between split panes
-   split_nav('move', 'h'),
-   split_nav('move', 'j'),
-   split_nav('move', 'k'),
-   split_nav('move', 'l'),
-   ---- resize panes
-   split_nav('resize', 'h'),
-   split_nav('resize', 'j'),
-   split_nav('resize', 'k'),
-   split_nav('resize', 'l'),
+  -- smart-splits
+  ---- move between split panes
+  split_nav('move', 'h'),
+  split_nav('move', 'j'),
+  split_nav('move', 'k'),
+  split_nav('move', 'l'),
+  ---- resize panes
+  split_nav('resize', 'h'),
+  split_nav('resize', 'j'),
+  split_nav('resize', 'k'),
+  split_nav('resize', 'l'),
 
-   -- panes: scroll pane
-   { key = 'u',        mods = mod.SUPER, action = act.ScrollByLine(-5) },
-   { key = 'd',        mods = mod.SUPER, action = act.ScrollByLine(5) },
-   { key = 'PageUp',   mods = 'NONE',    action = act.ScrollByPage(-0.75) },
-   { key = 'PageDown', mods = 'NONE',    action = act.ScrollByPage(0.75) },
+  -- panes: scroll pane
+  -- { key = 'u',        mods = mod.SUPER, action = act.ScrollByLine(-5) },
+  -- { key = 'd',        mods = mod.SUPER, action = act.ScrollByLine(5) },
+  { key = 'PageUp',   mods = 'NONE', action = act.ScrollByPage(-0.75) },
+  { key = 'PageDown', mods = 'NONE', action = act.ScrollByPage(0.75) },
 }
 
 for i = 1, 9 do
@@ -129,22 +129,22 @@ end
 
 -- stylua: ignore
 local key_tables = {
-   resize_pane = {
-      { key = 'h', action = act.AdjustPaneSize({ 'Left', 1 }) },
-      { key = 'j', action = act.AdjustPaneSize({ 'Down', 1 }) },
-      { key = 'k', action = act.AdjustPaneSize({ 'Up', 1 }) },
-      { key = 'l', action = act.AdjustPaneSize({ 'Right', 1 }) },
-      { key = 'Escape', action = 'PopKeyTable' },
-      { key = 'Enter', action = 'PopKeyTable' },
-   },
-   move_tab = {
-      { key = 'h', action = act.MoveTabRelative(-1) },
-      { key = 'j', action = act.MoveTabRelative(-1) },
-      { key = 'k', action = act.MoveTabRelative(1) },
-      { key = 'l', action = act.MoveTabRelative(1) },
-      { key = 'Escape', action = 'PopKeyTable' },
-      { key = 'Enter', action = 'PopKeyTable' },
-   },
+  resize_pane = {
+    { key = 'h',      action = act.AdjustPaneSize({ 'Left', 1 }) },
+    { key = 'j',      action = act.AdjustPaneSize({ 'Down', 1 }) },
+    { key = 'k',      action = act.AdjustPaneSize({ 'Up', 1 }) },
+    { key = 'l',      action = act.AdjustPaneSize({ 'Right', 1 }) },
+    { key = 'Escape', action = 'PopKeyTable' },
+    { key = 'Enter',  action = 'PopKeyTable' },
+  },
+  move_tab = {
+    { key = 'h',      action = act.MoveTabRelative(-1) },
+    { key = 'j',      action = act.MoveTabRelative(-1) },
+    { key = 'k',      action = act.MoveTabRelative(1) },
+    { key = 'l',      action = act.MoveTabRelative(1) },
+    { key = 'Escape', action = 'PopKeyTable' },
+    { key = 'Enter',  action = 'PopKeyTable' },
+  },
 }
 
 local mouse_bindings = {
