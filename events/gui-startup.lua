7de9e0f597171d1e35dff10c3d7eb65401c25596
noidilin <linganinja.0120@gmail.com>
2026-02-25 05:23:16 +0800

    feat(config): wezterm now launch with main workspace instead of default

--- events/gui-startup.lua
+++ events/gui-startup.lua
@@ -1,11 +1,20 @@
 local wezterm = require('wezterm')
 local mux = wezterm.mux
+local platform = require('utils.platform')
 
 local M = {}
 
 M.setup = function()
    wezterm.on('gui-startup', function(cmd)
-      local _, _, window = mux.spawn_window(cmd or {})
+      local spawn_opts = cmd or {}
+
+      if platform.is_mac then
+         spawn_opts.workspace = 'main'
+         spawn_opts.cwd = wezterm.home_dir
+         spawn_opts.args = spawn_opts.args or { '/opt/homebrew/bin/nu', '-l' }
+      end
+
+      local _, _, window = mux.spawn_window(spawn_opts)
       window:gui_window():maximize()
    end)
 end
