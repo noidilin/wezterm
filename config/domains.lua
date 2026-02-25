c823749f831ef700507c5ce23d5d1ea7fc1884bf
noidilin <linganinja.0120@gmail.com>
2026-02-25 22:34:40 +0800

    fix(domain): fix domain config for windows due to rebase resolution error

--- config/domains.lua
+++ config/domains.lua
@@ -2,33 +2,33 @@ local platform = require('utils.platform')
 
 local options = {
 	-- ref: https://wezfurlong.org/wezterm/config/lua/SshDomain.html
-	-- ssh_domains = {},
-	ssh_domains = {
-		-- yazi's image preview on Windows will only work if launched via ssh from WSL
-		{
-			name = 'wsl.ssh',
-			remote_address = 'localhost',
-			multiplexing = 'None',
-			default_prog = { 'fish', '-l' },
-			assume_shell = 'Posix',
-		},
-	},
+	ssh_domains = {},
 
 	-- ref: https://wezfurlong.org/wezterm/multiplexing.html#unix-domains
 	unix_domains = {},
 
 	-- ref: https://wezfurlong.org/wezterm/config/lua/WslDomain.html
-	wsl_domains = {},
-}
-
-if platform.is_win then
-	options.ssh_domains = {
+	wsl_domains = {
 		{
 			name = 'wsl.arch',
 			distribution = 'archlinux',
 			username = 'noid',
 			default_cwd = '/home/noid',
 			default_prog = { 'nu' },
+		},
+	},
+}
+
+if platform.is_win then
+	options.ssh_domains = {
+		-- yazi's image preview on Windows will only work if launched via ssh from WSL
+		{
+			name = 'wsl.ssh',
+			remote_address = 'localhost',
+			multiplexing = 'None',
+			default_cwd = '/home/noid',
+			default_prog = { 'nu' },
+			assume_shell = 'Posix',
 		},
 	}
 end
