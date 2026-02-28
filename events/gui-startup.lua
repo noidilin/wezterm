bd03e274b21bf920b1eb5b27fc73d9455a2b72df
noidilin <linganinja.0120@gmail.com>
2026-02-28 23:24:10 +0800

    feat(events): gui-startup is now platform agnostic and respect the passed in cmd args

--- events/gui-startup.lua
+++ events/gui-startup.lua
@@ -1,6 +1,5 @@
 local wezterm = require('wezterm') ---@type Wezterm
 local mux = wezterm.mux
-local platform = require('utils.platform')
 local custom = require('utils.custom')
 
 local M = {}
@@ -9,12 +8,11 @@ M.setup = function()
 	wezterm.on('gui-startup', function(cmd)
 		local spawn_opts = cmd or {}
 		local workspace = custom.get('name.workspace', 'main')
-		local default_shell = custom.get_platform('executable.default_shell', '/opt/homebrew/bin/nu')
 
-		if platform.is_mac then
-			spawn_opts.workspace = workspace
+		spawn_opts.workspace = spawn_opts.workspace or workspace
+
+		if spawn_opts.cwd == nil and spawn_opts.domain == nil then
 			spawn_opts.cwd = wezterm.home_dir
-			spawn_opts.args = spawn_opts.args or { default_shell, '-l' }
 		end
 
 		local _, _, window = mux.spawn_window(spawn_opts)
